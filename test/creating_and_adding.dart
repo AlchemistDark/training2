80c2f562700ec9f4958853204ba014823d57daca
kel <dmitry@bassg.com>
2021-12-09 01:47:32 +0300

    Added first test

---
+++ test/creating_and_adding.dart
@@ -0,0 +1,20 @@
+import 'package:one_way_list/sl_list.dart';
+import 'package:test/test.dart';
+
+main () {
+  test("Create empty", () {
+    final list = SLList();
+
+    // Проверим, что root нулевой (это мы внутренности копаем)
+    expect(list.root, isNull);
+
+    // Проверим, что длина нулевая
+    expect(list.lenghtR(), equals(0));
+  });
+
+  test("Adding makes length bigger", () {
+
+  });
+
+
+}
